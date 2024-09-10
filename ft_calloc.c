9b2daa1e064867ac75eb2fb531cb1b57ca7c36bd
Teerakarn Phoonsiri <tphoonsi@z2t13c3.42bangkok.com>
2024-09-10 15:07:28 +0700

    0000

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: tphoonsi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 12:53:26 by tphoonsi          #+#    #+#             */
-/*   Updated: 2024/09/10 15:01:24 by tphoonsi         ###   ########.fr       */
+/*   Updated: 2024/09/10 15:06:58 by tphoonsi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	*ft_calloc(size_t count, size_t size)
 	lenght = size * count;
 	if (count == 0 || size == 0)
 		return (malloc(0));
-	else if ((long)count < 0 || (long) size < 0)
+	else if ((long)count < 0 || (long)size < 0)
 		return (NULL);
 	ptr = (void *)malloc(lenght);
 	if (ptr == NULL)
